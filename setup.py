6c66cd53708498bc22143490410344e03200ebb4
Yann Tremblay alt <ytremblay@rhetorik.com>
2023-05-11 16:06:13 -0400

    cp

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import Extension, setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules=cythonize('src/**/*.pyx', compiler_directives={'language_level': '3'}, annotate=True)
+    ext_modules=cythonize('src/**/*.pyx', compiler_directives={'language_level': '3'}, annotate=True),
+    package_dir={'': 'src'}
 )
