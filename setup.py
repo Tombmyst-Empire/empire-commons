de46d9359ff6f2c99f106dfde7e78cf20e2e7f21
Yann Tremblay alt <ytremblay@rhetorik.com>
2023-06-11 20:46:06 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
-    name='empire',
+    name='empire_commons',
     ext_modules=cythonize('src/**/*.pyx', compiler_directives={'language_level': '3'}, annotate=True),
     package_dir={'': 'src'}
 )
